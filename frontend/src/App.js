62488e6fbe08e9b5553d1bf20192c8083b8f14c9
Tomer Ovadia <o.tomerr@gmail.com>
2018-12-26 16:14:48 +0200

    bed + fed - poc: fetch api's data using cors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      parents: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/parents')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.setState({ parents: data })
+          return data;
+        })
+        .catch(error => console.log(error));
+  }
+
+
   render() {
+    const { parents } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Just a POC
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <ul>
+            {parents.map(parent =>
+                <li key={parent.id}>
+                  <span>{parent.name}</span>
+                </li>
+            )}
+          </ul>
         </header>
       </div>
     );
