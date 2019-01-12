6d70c5e5959ce8be13d4c2522bd3ea13c9952bea
Tomer Ovadia <o.tomerr@gmail.com>
2019-01-13 01:32:57 +0200

    fed - config vars

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
 export const APP_NAME = 'wetasty';
 export const APP_SLOGAN = 'Eat What You Love...';
 export const API_URL =
-  location.hostname === 'localhost'
+  window.location.hostname === 'localhost'
     ? '//localhost:3001/api/v1/'
     : '//wetasty-api.herokuapp.com/api/v1/';
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyCV3WpBv_f6mh0ZSW-y1IbacB8CxY6aDRQ';
