8901d01d5f375dfd254d047d1a6b63738f0c81d3
Tomer Ovadia <o.tomerr@gmail.com>
2019-01-12 23:54:46 +0200

    package.json change

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,7 @@
 export const APP_NAME = 'wetasty';
 export const APP_SLOGAN = 'Eat What You Love...';
-export const API_URL = '//localhost:3001/api/v1/';
+// export const API_URL = '//localhost:3001/api/v1/';
+export const API_URL = '//wetasty-api.herokuapp.com/api/v1/';
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyCV3WpBv_f6mh0ZSW-y1IbacB8CxY6aDRQ';
 export const RANDOM_IMAGE_LP_URL = 'http://lorempixel.com/246/140/food/';
 export const RESTAURANT_MAX_RATING = 3;
